fc78931bbe048f393089ff3ea596099c0a0fedbd
TARISO <toni.ariso@gmail.com>
2020-09-07 07:56:48 +0200

    postman

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ dotenv.config({ path: './config.env' });
 
 const app = express();
 
+// Dev logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // Profile routes
 app.use('/api/v1/profile', require('./routes/profile'));
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(
-    `[+] Server running in ${process.env.NODE_ENV} mode on port ${port}`
-  );
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
